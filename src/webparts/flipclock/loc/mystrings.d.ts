6c1de5babf0fb877b98ca3a4cd79b222cf2512fb
Eric <eric.veliyulin@uninett.no>
2019-05-23 11:32:27 +0200

    Add placeholder flipclock webpart

---
+++ src/webparts/flipclock/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFlipclockWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FlipclockWebPartStrings' {
+  const strings: IFlipclockWebPartStrings;
+  export = strings;
+}
