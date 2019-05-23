6dace0ee3a9086d77fecf87aee502400e227edea
Eric <eric.veliyulin@uninett.no>
2019-05-23 14:37:04 +0200

    Replace all files with files from fresh SPFX 1.8.1 project

--- src/webparts/socialLinks/SocialLinksWebPart.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-    socialLinks: string;
-    container: string;
-    row: string;
-    column: string;
-    'ms-Grid': string;
-    socialContent: string;
-};
-export default styles;
