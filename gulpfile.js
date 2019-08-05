bfb515531b53be18287b658d8f14db604a44549c
Eric <eric.veliyulin@uninett.no>
2019-08-05 10:45:49 +0200

    Remove passwords from files

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const build = require('@microsoft/sp-build-web');
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin-legacy');
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+build.addSuppression(`Warning - [sass] The local CSS class 'ms-Pivot' is not camelCase and will not be type-safe.`);
 
 build.configureWebpack.setConfig({
     additionalConfiguration: function (config) {
